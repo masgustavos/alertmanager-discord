eb4666d6062eb91003b963aad48e7c042ba0aba2
Gustavo Stein <gustavosteinmattos@gmail.com>
2021-08-29 14:07:58 -0300

    Handle missing severity / Helm mock values.yaml (#2)
    - Improved the readability of the CheckIfHasOnlySeveritiesToIgnoreWhenAlone function
    - Adding the unknown severity when the alert does not have one
    - Adding example values.yaml files to use with Helm

--- alertmanager/alertmanager.go
+++ alertmanager/alertmanager.go
@@ -30,17 +30,20 @@ func ExtractBodyInfo(alertmanagerBody MessageBody, config config.Config) Message
 			alert.GeneratorURL = urlRegex.ReplaceAllString(alert.GeneratorURL, config.PrometheusURL+"/graph")
 		}
 
+		severityValue, ok := alert.Labels[config.Severity.Label]
+		if ok {
+			countBySeverity[severityValue]++
+		} else {
+			alert.Labels[config.Severity.Label] = "unknown"
+			countBySeverity["unknown"]++
+		}
+
 		if status == "firing" {
 			firingCount++
 			firingAlertsGroupedByName[alertName] = append(firingAlertsGroupedByName[alertName], alert)
 		} else if status == "resolved" {
 			resolvedCount++
 			resolvedAlertsGroupedByName[alertName] = append(resolvedAlertsGroupedByName[alertName], alert)
-		}
-
-		severityValue, ok := alert.Labels[config.Severity.Label]
-		if ok {
-			countBySeverity[severityValue]++
 		}
 
 	}
@@ -74,18 +77,19 @@ func CheckIfHasOnlySeveritiesToIgnoreWhenAlone(
 	}
 
 	for severity := range countBySeverity {
-		severityIsInIgnoreArray := false
-
-		for _, severityToIgnore := range severitiesToIgnore {
-			if severityToIgnore == severity {
-				severityIsInIgnoreArray = true
-			}
-		}
-
-		if !severityIsInIgnoreArray {
+		if !contains(severitiesToIgnore, severity) {
 			return false
 		}
 	}
 
 	return true
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
